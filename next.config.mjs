fcfedb7f9d58390171e08d0ccb5bd5d2efedf337
vicsnet <adesanmiadebisi@gmail.com>
2025-05-29 12:32:18 +0100

    chore: next.config buildID added

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['gateway.lighthouse.storage', 'emerald-big-beaver-890.mypinata.cloud', '/'],
-      }
+  images: {
+    domains: ['gateway.lighthouse.storage', 'emerald-big-beaver-890.mypinata.cloud', '/'],
+  },
+  generateBuildId: async () => {
+    // This ensures a unique build ID on every deploy to prevent stale cache
+    return Date.now().toString();
+  },
 };
 
 export default nextConfig;
